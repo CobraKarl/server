b5969155d70b806801268ac6d5192964cc80e258
MacMike <92017058+CobraKarl@users.noreply.github.com>
2022-01-27 10:21:56 +0100

    del12

--- Server.js
+++ Server.js
@@ -3,6 +3,7 @@ import express from "express"
 import dotenv from "dotenv"
 import helmet from "helmet"
 import morgan from "morgan"
+import mongoose  from "mongoose"
 import middelwares from "./src/middlewares/Middlewares.js"
 
 
@@ -21,6 +22,13 @@ app.get("/recipe", (req, res) => {
 app.use(middelwares.notFound)
 app.use(middelwares.errorHandler)
 
+mongoose.connect ("mongodb://localhost/testdatabase", {useNewUrlParser: true, useUnifiedTopology: true})
+.then (() => console.log ("Successfully connected to the database"))
+.catch((error) => {
+    console.log ("ERROR TRYING TO CONNECT", error)
+    process.exit()    
+
+})
 
 app.listen(port, () => {
     console.log("Servern är igång på port ${port}")
