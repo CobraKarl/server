d10a7922f87dc16f43224e66c9d12c0082902296
MacMike <92017058+CobraKarl@users.noreply.github.com>
2022-01-27 14:39:11 +0100

    del 16

---
+++ src/controllers/User.controller.js
@@ -0,0 +1,21 @@
+import UserModel from "../moduls/User.model.js";
+
+const creatUser = async (req, res) => {
+    const user = new UserModel ({
+       username: req.body.username,
+       password: req.body.password
+    })
+
+    try {
+    const respons = user.save()
+    res.status(201).send(respons)
+
+    } catch (error) {
+    res.status(500).send({ message: error.message})
+    }
+
+}
+
+export default {
+    creatUser
+}
