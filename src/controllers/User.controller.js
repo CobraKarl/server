3b78d7d31f55de46f93d03a7aa194d2639399630
MacMike <92017058+CobraKarl@users.noreply.github.com>
2022-01-31 14:34:18 +0100

    del22

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 import UserModel from "../moduls/User.model.js";
 
 const creatUser = async (req, res) => {
@@ -37,8 +38,25 @@ const getUserWithId = async (req, res) => {
     }
 }
 
+const getUserWithUsernameQuery = async (req, res) => {
+    try {
+        const respons = await UserModel.find({username: req.query.username})
+        respons.length !== 0 
+            ? res.status(200).send(respons) 
+            : res.status(404).send({ message: "Could not find user with username: " + req.query.username })
+
+    } catch (error) {
+        res.status(500).send ({
+            message: "Error occured while trying to retrive user with username: " + req.query.puserId,
+            error: error.message
+        })
+
+    }
+}
+
 export default {
     creatUser,
     getAllUsers,
-    getUserWithId
+    getUserWithId,
+    getUserWithUsernameQuery
 }
--- src/routes/User.route.js
+++ src/routes/User.route.js
@@ -4,6 +4,7 @@ const routes =(app) => {
     app.post ("/user", UserController.creatUser)
     app.get ("/user", UserController.getAllUsers)
     app.get ("/user/:userId", UserController.getUserWithId)
+    app.get ("/searchuser", UserController.getUserWithUsernameQuery)
 }
 
 export default {
