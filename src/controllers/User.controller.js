e63433d61780849723e66d4cd0d4a19eafef0a81
MacMike <92017058+CobraKarl@users.noreply.github.com>
2022-02-02 11:23:55 +0100

    del25

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -1,5 +1,6 @@
 import { response } from "express";
 import UserModel from "../moduls/User.model.js";
+import StatusCode from "../../configuration/StatusCode.js";
 
 const creatUser = async (req, res) => {
     const user = new UserModel ({
@@ -9,19 +10,19 @@ const creatUser = async (req, res) => {
 
     try {
     const respons = await user.save()
-    res.status(201).send(respons)
+    res.status(StatusCode.CREATED).send(respons)
 
     } catch (error) {
-    res.status(500).send({ message: error.message})
+    res.status(StatusCode.INTERNAL_SERVER_ERROR).send({ message: error.message})
     }
 
 }
 const getAllUsers = async (req, res) => {
     try {
         const respons = await UserModel.find()
-        res.status (200).send(respons)
+        res.status (StatusCode.OK).send(respons)
     } catch (error) {
-        res.status(500).send ({ message: error.message })
+        res.status(StatusCode.INTERNAL_SERVER_ERROR).send ({ message: error.message })
     }
 
 }
@@ -29,9 +30,9 @@ const getAllUsers = async (req, res) => {
 const getUserWithId = async (req, res) => {
     try {
         const respons = await UserModel.findById(req.params.userId)
-        res.status(200).send(respons)
+        res.status(StatusCode.OK).send(respons)
     } catch (error) {
-        res.status(500).send({
+        res.status(StatusCode.INTERNAL_SERVER_ERROR).send({
             message: "Error occured while trying to retrive user with ID:" + req.params.userId,
             error: error.message
         })
@@ -42,11 +43,11 @@ const getUserWithUsernameQuery = async (req, res) => {
     try {
         const respons = await UserModel.find({username: req.query.username})
         respons.length !== 0 
-            ? res.status(200).send(respons) 
-            : res.status(404).send({ message: "Could not find user with username: " + req.query.username })
+            ? res.status(StatusCode.OK).send(respons) 
+            : res.status(StatusCode.NOT_FOUND).send({ message: "Could not find user with username: " + req.query.username })
 
     } catch (error) {
-        res.status(500).send ({
+        res.status(StatusCode.INTERNAL_SERVER_ERROR).send ({
             message: "Error occured while trying to retrive user with username: " + req.query.puserId,
             error: error.message
         })
@@ -56,14 +57,14 @@ const getUserWithUsernameQuery = async (req, res) => {
 
 const updateUser = async (req, res) => {
     try {
-        if (!req.body) {return res.status(400).send({ message: "cannot update empy values" }) }
+        if (!req.body) {return res.status(StatusCode.BAD_REQUEST).send({ message: "cannot update empy values" }) }
         const respons = await UserModel.findByIdAndUpdate(req.params.userId, {
             username: req.body.username,
             password: req.body.password
         }, { new: true})
-        res.status(200).send(respons)
+        res.status(StatusCode.OK).send(respons)
     } catch (error) {
-        res.status(500).send({
+        res.status(StatusCode.INTERNAL_SERVER_ERROR).send({
             message: "Error occures while trying to update values of the user with ID: " + req.params.userId,
             error: error.message
         })
@@ -74,11 +75,11 @@ const updateUser = async (req, res) => {
 const deleteUser = async (req, res) => {
     try {
         const respons = await UserModel.findByIdAndDelete(req.params.userId)
-        res.status(200).send ({
+        res.status(StatusCode.OK).send ({
             message: `Sucessfully deleted the USER with username: ${respons.username} and ID: ${req.params.userId}`
         })
     } catch (error){
-        res.status(500).send ({
+        res.status(StatusCode.INTERNAL_SERVER_ERROR).send ({
             message: "Error occured while trying to delete user with ID: " + req.params.userId,
             error: error.message
         })
