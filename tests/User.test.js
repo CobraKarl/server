b369c61ca39be5a0b81f2f824833b38ea61105ec
MacMike <92017058+CobraKarl@users.noreply.github.com>
2022-02-04 11:43:42 +0100

    del 27, sista delen, uppgiften klar

--- tests/User.test.js
+++ tests/User.test.js
@@ -1,5 +1,6 @@
 import Chai from "chai"
 import ChaiHTTP from "chai-http"
+import { response } from "express"
 import {describe, it as test} from "mocha"
 import StatusCode from "../configuration/StatusCode.js"
 import app from "../Server.js"
@@ -8,6 +9,11 @@ Chai.should()
 Chai.use(ChaiHTTP)
 
 const randomString = Math.random().toString(36).substring(7)
+const userId ="61f7a8ae69509674e69fd75e"
+const user = {
+    username: randomString,
+    password: randomString, 
+}
 
 const testingNonExistingRoute = () => {
     describe("Testing a route that not exist", () => {
@@ -23,7 +29,86 @@ const testingNonExistingRoute = () => {
     })
 }
 
+const creatUser = () => {
+    describe("Testing CREATE(POST) method for user entity", () =>{ 
+        test ("Expecting a user to be created", (done) => {
+            Chai.request(app)
+                .post("/user")
+                .send(user)
+                .end ((error, respons) => {
+                    respons.should.have.a.status(StatusCode.CREATED)
+                    respons.body.should.be.a("object")
+                    respons.body.should.have.property("username").eq(user.username)
+                    respons.body.should.have.property("password").eq(user.password)
+                    done()
+
+                })
+        })
+
+    })
+
+}
+
+const getAllUsers = () => {
+    describe("Fetching all users(GET)", () => {
+        test ("Expecting to return all the users", (done) => {
+            Chai.request(app)
+                .get("/user")
+                .end((error, respons) => {
+                respons.should.have.status(StatusCode.OK)
+                respons.body.should.be.a("array")
+                respons.body.length.should.be.eq(respons.body.length)
+                done()
+            })
+        })
+
+    })
+}
+const updateUser = () => {
+    describe("Updating(PUT) a user in the database", () =>{
+        test ("Expecting a user to be updated", (done) =>{
+            Chai.request(app)
+                .put(`/user/${userId}`)
+                .send(user)
+                .end((error, respons) => {
+                    respons.should.have.status(StatusCode.OK)
+                    respons.body.should.be.a("object")
+                    respons.body.should.have.property("_id").eq(userId)
+                    respons.body.should.have.property("username").eq(user.username)
+                    respons.body.should.have.property("password").eq(user.password)
+                    done()
+                })
+
+        })
+
+    })
+
+
+}
+const deleteUser = () => {
+    describe("Deleting(DELETE) a user in the database", () =>{
+        test ("Expecting a user to be deleted", (done =>{
+            Chai.request(app)
+                .delete(`/user/${userId}`)
+                .end((error, respons) => {
+                    respons.should.have.status(StatusCode.OK)
+                    done()
+                
+
+            })
+        }))
+
+    })
+
+
+}
+
+
 describe("TESTING THE USER_API ROUTE", () => {
     testingNonExistingRoute()
+    creatUser()
+    getAllUsers()
+    updateUser()
+    deleteUser()
 
 })
